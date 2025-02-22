fcada425ca89e4efc4068ab9ece3ba5bf15f565a
Aditya Borikar <adityaborikar2@gmail.com>
2025-02-21 19:48:28 -0800

    Add policy fetching

--- app.py
+++ app.py
@@ -13,14 +13,33 @@ load_dotenv()
 app = Flask(__name__)
 csrf = CSRFProtect(app)
 
-supabase_client = None
-
 def create_supabase_client():
     url = os.environ.get("SUPABASE_PROJECT_URL")
     key = os.environ.get("SUPABASE_API_KEY")
     supabase_client = create_client(url, key)
+    return supabase_client
 
 create_supabase_client()
+
+
+@app.route('/api/startups', methods=['GET'])
+def get_startups():
+    supabase_client = create_supabase_client()
+    result = supabase_client.table("yc-startups").select("*").execute()
+    if result:
+        startups = result.data
+        startups = [
+            {
+                "name": startup["name"],
+                "yc_batch": startup["yc_batch"],
+                "team_size": startup["teamsize"],
+                "website": startup["website"]
+            }
+            for startup in startups
+        ]
+    else:
+        startups = []
+    return jsonify(startups)
 
 @app.route("/")
 def home():
